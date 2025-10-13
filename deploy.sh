f8b813f8c5fd2e27945fbeb2f478d8a0cae4d240
moodpatcher <moodpatcher@gmail.com>
2025-10-13 01:55:14 +0000

    fixed deploy command

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ docker run -d --name mcmp \
     --restart always \
     node:latest \
     bash -c '
-      apt update && apt install -y tmux iputils-ping iproute2 tcpdump && tmux new -d -s mcmp "cd /opt/mcmp && npm install && npm run dev" && tail -f /dev/null;
+      apt update && apt install -y tmux && tmux new -d -s mcmp "cd /opt/mcmp && npm install && npm run start" && tail -f /dev/null;
     '
